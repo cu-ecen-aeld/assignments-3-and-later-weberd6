1f4148d927a694b1ae6055e576c29be5d0ca5d91
Doug Weber <dweber641@gmail.com>
2023-09-26 16:10:30 -0400

    Add linux/slab.h include

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -18,6 +18,7 @@
 #include <linux/cdev.h>
 #include <linux/mutex.h>
 #include <linux/fs.h> // file_operations
+#include <linux/slab.h>
 #include "aesd-circular-buffer.h"
 #include "aesdchar.h"
 int aesd_major =   0; // use dynamic major
